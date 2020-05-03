9e9f6c446ff0799bd7888f8e75eb7f519481627d
KenMwaura1 <kemwaura@gmail.com>
2020-05-03 18:07:41 +0300

    added company profile,ceo and quarterly revenue

--- automation.py
+++ automation.py
@@ -9,7 +9,7 @@ def getdata(stock):
     # Balance Sheet group of items
     BS = requests.get(f"https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/{stock}?period=quarter")
     BS = BS.json()
-    # print(company_quote, share_price, BS)
+    print(company_quote, share_price, BS)
 
     # Get Total Company debt
     debt = float("{0:.2f}".format(float(BS['financials'][0]['Total debt'])/10**9))
@@ -18,7 +18,7 @@ def getdata(stock):
     cash = float("{0:.2f}".format(float(BS['financials'][0]['Cash and short-term investments'])/10**9))
 
     # Income Statement Group of Items
-    IS = requests.get(f"https://financialmodelingprep.com/v3/financials/income-statement/{stock}?period=quarter")
+    IS = requests.get(f"https://financialmodelingprep.com/api/v3/financials/income-statement/{stock}?period=quarter")
     IS = IS.json()
 
     #Most Recent Quarterly revenue
@@ -30,8 +30,10 @@ def getdata(stock):
 
     #CEO
     ceo = company_info['profile']['ceo']
-    
-    return (share_price, cash, debt, qRev, ceo)
 
-getdata('AAPL')
+    print(share_price, cash, debt, qRev, ceo)
+    return (share_price, cash, debt, qRev, ceo)
+   
+
+getdata('MSFT')
 
